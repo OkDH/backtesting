1cab16af12ce44676de4656cc229c2a4ca363276
ocko112 <ocko112@gmail.com>
2024-01-21 23:09:13 +0900

    백테스팅 테스트

--- main.py
+++ main.py
@@ -1,1 +1,76 @@
-print("gd")
+import yfinance as yf
+import backtrader as bt
+import datetime
+
+class TestStrategy(bt.Strategy):
+
+    def log(self, txt, dt=None):
+        ''' Logging function fot this strategy'''
+        dt = dt or self.datas[0].datetime.date(0)
+        print('%s, %s' % (dt.isoformat(), txt))
+
+    def __init__(self):
+        # Keep a reference to the "close" line in the data[0] dataseries
+        self.dataclose = self.datas[0].close
+
+    def notify_order(self, order):
+        if order.status in [order.Submitted, order.Accepted]:
+            return
+        if order.status in [order.Completed]:
+            if order.isbuy():
+                self.log(f'BUY  : 주가 {order.executed.price:,.0f}, '
+                    f'수량 {order.executed.size:,.0f}, '
+                    f'수수료 {order.executed.comm:,.0f}, '        
+                    f'자산 {cerebro.broker.getvalue():,.0f}')
+                self.buyprice = order.executed.price
+                self.buycomm = order.executed.comm
+            else:
+                self.log(f'SELL : 주가 {order.executed.price:,.0f}, '
+                    f'수량 {order.executed.size:,.0f}, '
+                    f'수수료 {order.executed.comm:,.0f}, '
+                    f'자산 {cerebro.broker.getvalue():,.0f}')
+            self.bar_executed = len(self)
+        elif order.status in [order.Canceled]:
+            self.log('ORDER CANCELD')
+        elif order.status in [order.Margin]:
+            self.log('ORDER MARGIN')
+        elif order.status in [order.Rejected]:
+            self.log('ORDER REJECTED')
+        # self.order = None
+    
+    def next(self):
+        # Simply log the closing price of the series from the reference
+        # self.log('Close, %.2f' % self.dataclose[0])
+
+        if self.dataclose[0] < self.dataclose[-1]:
+            # current close less than previous close
+
+            if self.dataclose[-1] < self.dataclose[-2]:
+                # previous close less than the previous close
+
+                # BUY, BUY, BUY!!! (with all possible default parameters)
+                self.log('BUY CREATE, %.2f' % self.dataclose[0])
+                self.order = self.buy(size=5)
+
+
+if __name__ == '__main__':
+
+    stock = yf.download("TQQQ", start="2021-01-01", end="2023-12-31")
+
+    cerebro = bt.Cerebro()
+
+    data = bt.feeds.PandasData(dataname=stock)
+
+    cerebro.adddata(data)
+    cerebro.addstrategy(TestStrategy)
+
+    cerebro.broker.setcash(100000.0)
+    cerebro.broker.setcommission(0.007)
+
+    print('Starting Portfolio Value: %.2f' % cerebro.broker.getvalue())
+
+    cerebro.run()
+
+    print('Final Portfolio Value: %.2f' % cerebro.broker.getvalue())
+
+    # cerebro.plot(style='candle', barup='red', bardown='blue')
